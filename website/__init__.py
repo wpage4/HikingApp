3eb71ba1f65c9f1f932cfe3cdbd986328e99b74e
wpage4 <wpage@eou.edu>
2022-05-08 18:24:19 -0700

    everything but database

--- website/__init__.py
+++ website/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_Key'] = 'wyattsApp'
+    app.config['SECRET_KEY'] = 'wyattsApp'
 
     from .views import views
     from .auth import auth
--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
@@ -12,4 +12,21 @@ def logout():
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if  request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(firstName) < 2:
+            flash('First name must be greater than 1 characters. ', category='error')
+        elif password1 != password2:
+            flash('Passwords dont match. ', category='error')
+        elif len(password1) < 7:
+            flash('Password should be more than than 6 characters. ', category='error')
+        else:
+            flash('Account Created!', category='success')
+
     return render_template("sign_up.html")  
